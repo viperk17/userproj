b69f134d6d3f422f34009ee5c35156d01025bfad
Prashant Singh <pk.airsqn@protonmail.com>
2020-08-12 13:35:12 +0530

    linked views

--- home/views.py
+++ home/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+def index(request):
+    render(request, 'index.html')
+
+def login(request):
+    render(request, 'login.html')
+
+def logout(request):
+    render(request, 'logout.html')
+ 
