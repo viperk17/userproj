6a279296cf4fbf821c08648273a40637817c3ae1
Prashant Singh <pk.airsqn@protonmail.com>
2020-08-12 13:48:42 +0530

    added allowed host

--- userproj/settings.py
+++ userproj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ck0#yqvv)-gaajcia*aob!r4cli6us2d1t7iuqhlzbnpy&lzai'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
