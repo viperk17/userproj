f9d8b9322d3760dd9ebbd96749f8cdf9027243dc
Prashant Singh <pk.airsqn@protonmail.com>
2020-08-13 14:52:31 +0530

    login

--- home/views.py
+++ home/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+
 
 # password for test user is Qwerty@1
 
@@ -7,6 +9,11 @@ def index(request):
     return render(request, 'index.html')
 
 def login(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        #check if user has entered the correct information
+        user = authenticate(username='viper', password='viper')
     return render(request, 'login.html')
 
 def logout(request):
